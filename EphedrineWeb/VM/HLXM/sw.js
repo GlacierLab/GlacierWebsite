309aa1ecfab65738a3f3a77e82e3d255a98be7dc
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-11-21 21:31:52 +1000

    更新版本

---
+++ EphedrineWeb/VM/HLXM/sw.js
@@ -0,0 +1,77 @@
+var APP_PREFIX = '红楼续梦'
+var VERSION = '20220704'
+var VERSION_AZUSA_PATCH_USE = 'ForceUpdate'
+var AZUSA_PATCH_SKIP_LIST = []
+var CACHE_NAME = APP_PREFIX + VERSION
+var AZUSA_CACHE = APP_PREFIX + VERSION_AZUSA_PATCH_USE
+var URLS = [
+    './'
+]
+const getCacheName = url => {
+    return CACHE_NAME;
+}
+self.addEventListener('fetch', event => {
+    if (event.request.url.indexOf("getVersionWorker") > 0) {
+        event.respondWith(new Response(VERSION));
+        return;
+    }
+    if (event.request.method == "GET" && (event.request.url.indexOf("http") == 0) && (event.request.url.indexOf("ForceNoCache") == -1)) {
+        event.respondWith(
+            caches.open(getCacheName(event.request.url)).then(async cache => {
+                return cache.match(event.request).then(response => {
+                    return response || fetch(event.request).then(response => {
+                        if (response.status < 400) {
+                            cache.put(event.request, response.clone());
+                            console.log('file cached : ' + event.request.url)
+                        }
+                        return response;
+                    }).catch(error => {
+                        console.log("failed to fetch :" + event.request.url)
+                        console.log(error);
+                    });
+                });
+            })
+
+        );
+    } else {
+        event.respondWith(fetch(event.request))
+    }
+});
+self.addEventListener('install', e => {
+    self.skipWaiting();
+    const install = async () => {
+        const cache = await caches.open(CACHE_NAME)
+        console.log('installing cache : ' + CACHE_NAME)
+        if ((await caches.has(AZUSA_CACHE))) {
+            console.log("Found Old Cache! Azusa Patch Working...");
+            caches.open(AZUSA_CACHE).then(oldCache => {
+                AZUSA_PATCH_SKIP_LIST.forEach(async url => {
+                    let tempResponse = await oldCache.match(url);
+                    if (tempResponse) {
+                        console.log("Azusa Success Transfer Old Cache : " + url)
+                        cache.put(url, tempResponse);
+                    }
+                })
+            })
+        }
+        await cache.addAll(AZUSA_PATCH_SKIP_LIST.concat(URLS));
+        return true;
+    }
+    e.waitUntil(install());
+});
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            var cacheWhitelist = keyList.filter(key => {
+                return key.indexOf(APP_PREFIX)
+            })
+            cacheWhitelist.push(CACHE_NAME);
+            return Promise.all(keyList.map((key, i) => {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i])
+                    return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+});
--- VM/Detail/HLXM.md
+++ VM/Detail/HLXM.md
@@ -8,17 +8,32 @@
 
 |镜像作者|琴梨梨|
 |  ----  | ----  |
-|镜像大小|1.70G  |
-|镜像SHA256|0efb9bbfb88607f65a1a05758ec54beb8b68315d832c7b4b5dd2dd83d1535430|
+|镜像版本|20221121|
+|镜像大小|1.55G  |
+|镜像SHA256|7a07324e51fee11446315e6d70bd7adea8fc760b995b21f6578bf9d6200b871e|
 |游戏基线版本|[CD版](https://www.ppxclub.com/708783-1-1)|  
 |系统基线版本|Windows XP ZH-TW SP3|
 |虚拟机版本|[Virtual Box 5.2.44](https://download.virtualbox.org/virtualbox/5.2.44/)|
 
 
-## 镜像下载
 
+<details><summary>历史版本</summary>
+
+#### 20220630
+|镜像作者|琴梨梨|
+|  ----  | ----  |
+|镜像大小|1.70G  |
+|镜像SHA256|0efb9bbfb88607f65a1a05758ec54beb8b68315d832c7b4b5dd2dd83d1535430|
+</details>
+
+## 镜像下载
+[Github Release](https://github.com/GlacierLab/.github/releases/download/%E7%BA%A2%E6%A5%BC%E7%BB%AD%E6%A2%A6%E4%BA%AC%E5%8D%8E%E9%A3%8E%E4%BA%91/HLXM_WINXP_20221121.7z)  
+
+Ephedrine安装助手：开发中
+<details><summary>历史版本</summary>
+
+#### 20220630
 [Github Release](https://github.com/GlacierLab/.github/releases/download/%E7%BA%A2%E6%A5%BC%E7%BB%AD%E6%A2%A6%E4%BA%AC%E5%8D%8E%E9%A3%8E%E4%BA%91/HLXM_WINXP_20220630.7z)  
 [城通](http://share.qinlili.bid/f/8067059-605678554-35b789?p=547873715)密码：547873715，可使用[解析器](https://ctfile.qinlili.bid)  
-[天翼](https://cloud.189.cn/web/share?code=rYruMz6VzMB3)访问码：eip2,可使用[云解析模式](https://disk189.qinlili.bid/file/Cloud189_User1/?id=11411116960708854)(若出错为服务端休眠唤醒，可等待半分钟后刷新)  
-  
-Ephedrine安装助手：开发中
+[天翼](https://cloud.189.cn/web/share?code=rYruMz6VzMB3)访问码：eip2
+</details>
